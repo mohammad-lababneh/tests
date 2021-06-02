7cf393b0741d56d5340390b8f1febbd074a9f2b6
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-06-03 00:48:48 +0300

    add

---
+++ app.js
@@ -0,0 +1,17 @@
+let container = document.getElementById('container');
+    let table = document.createElement('table');
+    container.appendChild(table);
+
+
+    let tr = document.createElement('tr');
+table.appendChild(tr);
+let td = document.createElement('td');
+tr.appendChild(td);
+td.textContent = ' Order Image ';
+let td1 = document.createElement('td');
+tr.appendChild(td1);
+td1.textContent = ' Order details ';
+
+
+
+
